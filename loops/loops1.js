b15d95929d110fa8d4690d22c71034bbf71166b0
Adam Ross <adamgarrettross@gmail.com>
2016-06-25 23:23:19 -0500

    Js

---
+++ loops/loops1.js
@@ -0,0 +1,35 @@
+// loops can execute a block of code as many times as yo want.
+
+var arr = [
+  "the first thing",
+  "php ",
+  " ruby ",
+  " js ",
+  " something ",
+  " new last thing "
+];
+//
+// var x = document.getElementById('test');
+//
+//
+//
+// i = 0;
+// while(i < arr.length)
+// { if (arr[i] === " ruby ")
+//     x.innerHTML += " I tell ya what, I love this language. == ";
+//   x.innerHTML += arr[i] + "<br>"
+//
+//   i++;
+// };
+
+var test = document.getElementById('test');
+var a = document.getElementById('basketball').getElementsByTagName("li");
+var b = document.getElementById('football').getElementsByTagName("li");
+
+var i = 0;
+
+  while(i < list.length)
+    {
+      test.innerHTML += list[i].innerHTML + "<br>";
+      i++;
+    };
