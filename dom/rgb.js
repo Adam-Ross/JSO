c71b36a3f96252106f1fbdbfaa9661c85a5068cd
Adam Ross <adamgarrettross@gmail.com>
2016-07-01 20:39:48 -0500

    Refactored.

--- dom/rgb.js
+++ dom/rgb.js
@@ -1,4 +1,28 @@
-var div = document.createElement('DIV');
+
+function fullScreen(element) {
+  var newElement = document.createElement(element);
+  newElement.style.height = '100vh';
+  document.body.appendChild(newElement);
+  return newElement
+}
+
+function input(inputType, DOMelement, callback) {
+  DOMelement.addEventListener(inputType, function(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    callback(DOMelement, x, y);
+  });
+}
+
+function output(element, x, y) {
+  element.textContent = x + ', ' + y;
+  element.style.backgroundColor = 'rgb(' + x + ', ' + y + ', 100)';
+}
+
+input('mousemove', fullScreen('DIV'), output);
+
+
+/*var div = document.createElement('DIV');
 div.style.height = '100vh';
 document.body.appendChild(div);
 div.addEventListener('mousemove', function(event) {
@@ -8,3 +32,4 @@ div.addEventListener('mousemove', function(event) {
   div.textContent = x + ' ' + y;
   div.style.backgroundColor = 'rgb(' + x +', ' + y +', 100)';
 });
+*/
